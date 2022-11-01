2160266b6171ab28a3d6970bded61ae813feb705
Amar Deo <amar_deo41@hotmail.com>
2022-10-31 21:15:38 -0500

    changes to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-tutorial';
+export class AppComponent implements AfterViewInit {
+  // model
+  title = 'some-angular-tutoriala adasdasda';
+  inputValue: number = 5;
+
+  ngAfterViewInit () {
+    setInterval(() => {
+      console.log('inputValue ', this.inputValue);
+    }, 1000);
+  }
+
 }
