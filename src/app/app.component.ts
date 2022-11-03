690a1d9f4e1958e0eb1076c2e865f951fff11800
Amar Deo <amar_deo41@hotmail.com>
2022-11-02 21:23:45 -0500

    angular lifecycle hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,45 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnChanges, OnInit, DoCheck, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements AfterViewInit, OnChanges, OnInit, DoCheck, OnDestroy {
+  constructor () {
+    console.log('inside constructor');
+    // initializing
+    this.title = 'some-angular-tutoriala adasdasda';
+  }
+
   // model
-  title = 'some-angular-tutoriala adasdasda';
+  title: string;
   inputValue: number = 5;
 
   ngAfterViewInit () {
-    setInterval(() => {
-      console.log('inputValue ', this.inputValue);
+    setTimeout(() => {
+      this.inputValue = 100;
     }, 1000);
+    console.log('inside ngAfterViewInit');
+    setInterval(() => {
+      // console.log('inputValue ', this.inputValue);
+    }, 1000);
+  }
+
+  ngOnChanges () {
+    console.log('inside ngOnChanges');
+  }
+
+  ngDoCheck () {
+    console.log('inside ngDoCheck');
+  }
+
+  ngOnDestroy () {
+    console.log('inside ngOnDestroy');
+  }
+
+  ngOnInit () {
+    console.log('inside ngOnInit');
   }
 
 }
