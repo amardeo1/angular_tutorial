e16bb45460ccffb1149d831767d753ea65e12ac1
Amar Deo <amar_deo41@hotmail.com>
2022-11-04 21:31:27 -0500

    added heroes component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,10 @@
-import { Component, AfterViewInit, OnChanges, OnInit, DoCheck, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit, OnChanges, OnInit, DoCheck, OnDestroy {
-  constructor () {
-    console.log('inside constructor');
-    // initializing
-    this.title = 'some-angular-tutoriala adasdasda';
-  }
-
-  // model
-  title: string;
-  inputValue: number = 5;
-
-  ngAfterViewInit () {
-    setTimeout(() => {
-      this.inputValue = 100;
-    }, 1000);
-    console.log('inside ngAfterViewInit');
-    setInterval(() => {
-      // console.log('inputValue ', this.inputValue);
-    }, 1000);
-  }
-
-  ngOnChanges () {
-    console.log('inside ngOnChanges');
-  }
-
-  ngDoCheck () {
-    console.log('inside ngDoCheck');
-  }
-
-  ngOnDestroy () {
-    console.log('inside ngOnDestroy');
-  }
-
-  ngOnInit () {
-    console.log('inside ngOnInit');
-  }
-
+export class AppComponent {
+  title = 'angular-tutorial';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { HeroesComponent } from './heroes/heroes.component';
 
+// everything inside the module is part of your app
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
